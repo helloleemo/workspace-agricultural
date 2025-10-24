c5a9f51e44b061bb6398340893ed2c7424ae916f
helloleemo <leemo.tseng@gmail.com>
2025-10-24 14:14:30 +0800

    feat(route): integrate route into apps

--- apps/agri-frontend/src/app/order/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function orderPage () {
-    return (
-        <div>
-            <p>訂購頁面</p>
-        </div>
-    )
-}
--- apps/agri-frontend/src/app/page.tsx
+++ apps/agri-frontend/src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Index() {
    */
   return (
     
-    <nav>
+    <nav className="flex gap-5">
       <Link href="/about">關於我們</Link>
-      <Link href="/products/list">檢視所有農產品</Link>
-      <Link href="/products/list">登入註冊</Link>
-      <Link href="/products/list">購物車</Link>
-      {/* <Link href="/products/123">Product #123</Link> */}
+      <Link href="/products">檢視所有農產品</Link>
+      <Link href="/login">登入註冊</Link>
+      <Link href="/cart">購物車</Link>
+      <Link href="/products/123">單一產品頁面 #123</Link>
     </nav>
   );
 }
